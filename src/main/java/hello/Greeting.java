d8e3513824e8c7ac80d1b954e829136794f53b98
Chenhua Lu <lu.chen.hua@wowoohr.com>
2017-04-05 21:35:19 +0800

    Step 1: Simple REST

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,7 +1,17 @@
 package hello;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * Created by luchenhua on 05/04/2017.
  */
-public class Greeting {
+@Data
+@AllArgsConstructor
+public class Greeting implements Serializable {
+
+    private final long id;
+    private final String content;
 }
