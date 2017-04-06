309d4e3144ed2d1fafa16b44878c6c2707b46feb
Chenhua Lu <lu.chen.hua@wowoohr.com>
2017-04-06 19:38:28 +0800

    Add Guava & OKHTTP

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -3,14 +3,12 @@ package hello;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.io.Serializable;
-
 /**
  * Created by luchenhua on 05/04/2017.
  */
 @Data
 @AllArgsConstructor
-public class Greeting implements Serializable {
+public class Greeting {
 
     private final long id;
     private final String content;
